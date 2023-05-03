cf79a56c3ee84e2e0fcf2e3d7bbae747b65db116
nicolcab97 <132416094+nicolcab97@users.noreply.github.com>
2023-05-02 20:32:56 -0500

    Add files via upload
    personalizar

---
+++ index.php
@@ -0,0 +1,97 @@
+
+<?php
+include("head.php");
+?>
+
+
+<?php
+include("marco-right.php");
+?>
+   
+ <div style="float:left; width:60%; margin:60px auto; padding:1vw">
+   <div  style="width: 100%;">     
+    <div style="width: 100%; height:100%; margin: auto ; display:inline-block;" >
+       <div style=" width:100%; height: 100%; top:0; font-size:20px; text-align:center; float:left;">
+        	<h1>¿Quién es Nixon Ortiz?</h1>
+       		 <hr>
+             
+          <div id="posteo" style="text-align: left; font-size:0.5vw;">
+                Posteado por:  Nixon Ortiz<br>
+                Fecha: 2023-04-30 <br>
+          </div>
+        </div>
+    </div>
+  </div>
+  <div style="width: 100%; height:100%;" >
+           <div  style="text-align:justify; font-size: 20px;">
+                   <p class="eys-parrafo">¡Hola! Me complace presentarme, soy Ortiz Quiñonez<b> Nixon Aldair </b>, un estudiante de Ingeniería en Tecnologías de la Información en la Universidad Técnica Luis Vargas Torres. </p>
+                   <p class="eys-parrafo">Actualmente llevo 4 años estudiando esta carrera y he adquirido conocimientos y habilidades en diversas áreas de la tecnología, incluyendo programación, bases de datos, redes de computadoras, uso de frameworks, desarrollo digital, Asesoria Comercial y mucho más.</p>
+                   <p class="eys-parrafo">Desde que inicié mi carrera universitaria, he estado muy enfocado en mejorar mis habilidades y conocimientos, por lo que he participado en varios proyectos relacionados con la tecnología.</p>
+                   <p class="eys-parrafo">Uno de mis objetivos a largo plazo es convertirme en un profesional exitoso y respetado en el campo de la tecnología. Para lograrlo, estoy constantemente en busca de nuevas oportunidades de aprendizaje y crecimiento profesional, además de mantenerme actualizado sobre las últimas tendencias y avances en la industria.</p>
+                   <p class="eys-parrafo"> El primer lenguaje con el cual empece a programar en el primer ciclo de universidad fue <b><a href="https://es.wikipedia.org/wiki/C%2B%2B"> C++</a></b>  aprendi a codificar en pseudocodigo y luego en IDEs especializados</p>
+
+   <div id="blog-seccion2"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mis estudios</h2> 
+    
+
+
+                   <p class="eys-parrafo">Mis estudios secundarios los realice en la  Unidad Educativa del Milenio Simón Plata Torres, donde me gradue en el año 2019, como Bachiller en Ciencias, Durante mis estudios secundarios, tuve la oportunidad de adquirir una sólida formación académica y desarrollar habilidades en diferentes áreas del conocimiento..</p>
+                   <p class="eys-parrafo">En particular, el enfoque en ciencias me permitió comprender mejor el mundo que nos rodea y aprender a analizar y resolver problemas de manera crítica. También tuve la oportunidad de participar en varias actividades extracurriculares, incluyendo clubes de ciencia y tecnología, lo que me permitió explorar mi pasión por la innovación y la creatividad.</p>
+<p class="eys-parrafo"> Además de mis estudios, también participé en actividades deportivas y culturales, lo que me permitió desarrollar habilidades sociales y de liderazgo. Estas habilidades me han sido muy útiles en mi vida cotidiana y en mi carrera universitaria.</p>
+   <div id="blog-seccion3"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
+    
+
+
+                   <p  class="eys-parrafo">En particular, tengo experiencia en HTML, CSS, PHP, Java, C#, Laravel, Node JS, React, MySQL, Supabase y programación orientada a objetos..</p> 
+                   <p  class="eys-parrafo">Además, tengo habilidades en la creación de programas de escritorio, uso de Linux y en el desarrollo de contenido digital. También tengo experiencia en el ámbito de marketing y ventas, lo que me permite entender mejor las necesidades de los clientes y ofrecer soluciones adaptadas a sus requerimientos.</p>
+                   <p  class="eys-parrafo">Como parte de mi experiencia laboral, he trabajado en proyectos de desarrollo de aplicaciones web y móviles, lo que me ha permitido adquirir habilidades en programación no code y en el uso de herramientas de desarrollo de aplicaciones. También he trabajado en proyectos de marketing digital y ventas en línea, lo que me ha permitido entender mejor la dinámica del mercado y ofrecer soluciones efectivas y eficientes.</p>
+   <div id="blog-seccion4"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
+    
+
+                   <p   class="eys-parrafo">Soy amante a la lectura, así que en los ratos libres me encanta leer diferentes generos de libros tanto de filosofia o novelas de cualquier genero. Otros de mis pasatiendos es Desarrollar proyectos de programacion mediante cursos o tutoriales. </p>
+       
+
+                   
+
+               <hr>
+               <div style=" bottom: 0; margin-bottom: 0;   width:100%; display: inline-block;" >
+                  <div style="width: 40%;  float: left;">
+                   <a href="#">&#171;   Previo</a>
+                  </div>
+
+                  <div style="width: 40%; float: right; text-align: right;">
+                   <a href="apendiendo-cpp">Programa para aprender C++ &#187;</a>
+                  </div>
+
+                </div>
+           </div>
+  </div> 
+
+  </div>
+
+
+
+
+<?php
+include("marco-left.php")
+?>
+ 
+<script >  
+
+ document.title="Nixon Ortiz"; 
+document.getElementById("foto").src="./images/OrtizNixon.jpeg";
+document.getElementById("elnombre").innerHTML="Nixon Ortiz";
+
+ document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
+
+ document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='eys-kevin.php'>Kevin Aguilar</a></p><p><a href='eys-damaris.php'>Damaris Viallalva</a></p> <a href='eys-nixon.php'>Nixon Ortiz</a></p> ";
+ </script>
+ 
+<?php
+include("foot.php")
+?>
