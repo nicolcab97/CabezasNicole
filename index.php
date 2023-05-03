48aaab6a9891ce862aae0aa3bc07375fae99e442
nicolcab97 <132416094+nicolcab97@users.noreply.github.com>
2023-05-02 20:51:45 -0500

    Update index.php
    crear

--- index.php
+++ index.php
@@ -12,23 +12,23 @@ include("marco-right.php");
    <div  style="width: 100%;">     
     <div style="width: 100%; height:100%; margin: auto ; display:inline-block;" >
        <div style=" width:100%; height: 100%; top:0; font-size:20px; text-align:center; float:left;">
-        	<h1>¿Quién es Nixon Ortiz?</h1>
+        	<h1>¿Quién es Nicole Cabezas ?</h1>
        		 <hr>
              
           <div id="posteo" style="text-align: left; font-size:0.5vw;">
-                Posteado por:  Nixon Ortiz<br>
-                Fecha: 2023-04-30 <br>
+                Posteado por:  Nicole Cabezas <br>
+                Fecha: 2023-05-02<br>
           </div>
         </div>
     </div>
   </div>
   <div style="width: 100%; height:100%;" >
            <div  style="text-align:justify; font-size: 20px;">
-                   <p class="eys-parrafo">¡Hola! Me complace presentarme, soy Ortiz Quiñonez<b> Nixon Aldair </b>, un estudiante de Ingeniería en Tecnologías de la Información en la Universidad Técnica Luis Vargas Torres. </p>
-                   <p class="eys-parrafo">Actualmente llevo 4 años estudiando esta carrera y he adquirido conocimientos y habilidades en diversas áreas de la tecnología, incluyendo programación, bases de datos, redes de computadoras, uso de frameworks, desarrollo digital, Asesoria Comercial y mucho más.</p>
-                   <p class="eys-parrafo">Desde que inicié mi carrera universitaria, he estado muy enfocado en mejorar mis habilidades y conocimientos, por lo que he participado en varios proyectos relacionados con la tecnología.</p>
-                   <p class="eys-parrafo">Uno de mis objetivos a largo plazo es convertirme en un profesional exitoso y respetado en el campo de la tecnología. Para lograrlo, estoy constantemente en busca de nuevas oportunidades de aprendizaje y crecimiento profesional, además de mantenerme actualizado sobre las últimas tendencias y avances en la industria.</p>
-                   <p class="eys-parrafo"> El primer lenguaje con el cual empece a programar en el primer ciclo de universidad fue <b><a href="https://es.wikipedia.org/wiki/C%2B%2B"> C++</a></b>  aprendi a codificar en pseudocodigo y luego en IDEs especializados</p>
+                   <p class="eys-parrafo">¡Hola! Me presento, soy Cabezas Intriago<b> Nicole </b>, estudiante de Ingeniería en Tecnologías de la Información en la Universidad Técnica Luis Vargas Torres de Esmeraldas. </p>
+                   <p class="eys-parrafo">Actualmente Estoy en constante aprendizaje y exploración en el mundo de la tecnología, donde me he especializado en diversas áreas como la programación, el desarrollo web, la seguridad informática y las bases de datos.</p>
+                   <p class="eys-parrafo">Durante mi carrera universitaria he tenido la oportunidad de adquirir conocimientos especiales en el área de la tecnología, lo que me ha permitido desarrollar habilidades en el trabajo en equipo, la gestión de proyectos y la resolución de problemas.</p>
+                   <p class="eys-parrafo">Mi objetivo a largo plazo es convertirme en un profesional altamente capacitado y respetado en el campo de la tecnología, donde pueda aplicar mis habilidades y conocimientos en proyectos innovadores y contribuir al desarrollo de la sociedad.</p>
+                   <p class="eys-parrafo"> Para lograrlo, estoy en constante búsqueda de oportunidades de aprendizaje y crecimiento profesional, y me mantengo actualizado sobre las últimas tendencias y avances en la industria.</p>
 
    <div id="blog-seccion2"  style="height: 50px;"></div>
 
@@ -36,24 +36,24 @@ include("marco-right.php");
     
 
 
-                   <p class="eys-parrafo">Mis estudios secundarios los realice en la  Unidad Educativa del Milenio Simón Plata Torres, donde me gradue en el año 2019, como Bachiller en Ciencias, Durante mis estudios secundarios, tuve la oportunidad de adquirir una sólida formación académica y desarrollar habilidades en diferentes áreas del conocimiento..</p>
-                   <p class="eys-parrafo">En particular, el enfoque en ciencias me permitió comprender mejor el mundo que nos rodea y aprender a analizar y resolver problemas de manera crítica. También tuve la oportunidad de participar en varias actividades extracurriculares, incluyendo clubes de ciencia y tecnología, lo que me permitió explorar mi pasión por la innovación y la creatividad.</p>
-<p class="eys-parrafo"> Además de mis estudios, también participé en actividades deportivas y culturales, lo que me permitió desarrollar habilidades sociales y de liderazgo. Estas habilidades me han sido muy útiles en mi vida cotidiana y en mi carrera universitaria.</p>
+                   <p class="eys-parrafo">Mis estudios secundarios los realice en la Unidad Educativa Calderón 2, donde me gradue como Bachiller en Ciencias. Durante mi educación secundaria, adquirí conocimientos y habilidades en áreas como matemáticas, física, química, biología y ciencias sociales.</p>
+                   <p class="eys-parrafo">Estos estudios me permitieron entender mejor el mundo que nos rodea, analizar y resolver problemas de manera crítica y desarrollar habilidades de investigación y experimentación. .</p>
+<p class="eys-parrafo"> Como bachiller en ciencias, estoy interesado en seguir mi formación académica en áreas relacionadas con la tecnología, la ingeniería y las ciencias aplicadas</p>
    <div id="blog-seccion3"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
     
 
 
-                   <p  class="eys-parrafo">En particular, tengo experiencia en HTML, CSS, PHP, Java, C#, Laravel, Node JS, React, MySQL, Supabase y programación orientada a objetos..</p> 
-                   <p  class="eys-parrafo">Además, tengo habilidades en la creación de programas de escritorio, uso de Linux y en el desarrollo de contenido digital. También tengo experiencia en el ámbito de marketing y ventas, lo que me permite entender mejor las necesidades de los clientes y ofrecer soluciones adaptadas a sus requerimientos.</p>
-                   <p  class="eys-parrafo">Como parte de mi experiencia laboral, he trabajado en proyectos de desarrollo de aplicaciones web y móviles, lo que me ha permitido adquirir habilidades en programación no code y en el uso de herramientas de desarrollo de aplicaciones. También he trabajado en proyectos de marketing digital y ventas en línea, lo que me ha permitido entender mejor la dinámica del mercado y ofrecer soluciones efectivas y eficientes.</p>
+                   <p  class="eys-parrafo">En particular, Tengo conocmientos en el uso de lenguajes de programación como PHP y Javascript, así como en la creación de aplicaciones básicas utilizando HTML y CSS. También estoy familiarizado con la administración de sistemas y la resolución de problemas técnicos básicos.</p> 
+                   <p  class="eys-parrafo">Soy una persona proactiva y autodirigida, y me gusta mantenerme actualizado sobre las últimas tendencias y tecnologías en la industria. Actualmente, estoy trabajando en mejorar mis habilidades en programación y desarrollo web mediante cursos en línea y proyectos personales.</p>
+
    <div id="blog-seccion4"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
     
 
-                   <p   class="eys-parrafo">Soy amante a la lectura, así que en los ratos libres me encanta leer diferentes generos de libros tanto de filosofia o novelas de cualquier genero. Otros de mis pasatiendos es Desarrollar proyectos de programacion mediante cursos o tutoriales. </p>
+                   <p   class="eys-parrafo">Soy una persona apasionada por los hobbies que me permiten conectarme con mi lado creativo y relajarme después de un largo día de trabajo. En especial, disfruto de la música y la danza, ya que me permite expresarme de manera artística y mejorar mi condición física. También me encanta cocinar y experimentar con nuevas recetas, así como viajar y explorar nuevas culturas y lugares. Finalmente, soy un amante de los deportes y disfruto jugar fútbol y básquetbol con amigos y familiares. </p>
        
 
                    
@@ -83,9 +83,9 @@ include("marco-left.php")
  
 <script >  
 
- document.title="Nixon Ortiz"; 
-document.getElementById("foto").src="./images/OrtizNixon.jpeg";
-document.getElementById("elnombre").innerHTML="Nixon Ortiz";
+ document.title="Nicole Cabezas"; 
+document.getElementById("foto").src="./images/CabezasNicole.jpeg";
+document.getElementById("elnombre").innerHTML="Nicole Cabezas ";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
 
